0c562c46cd24a64a2169ccb942444032e6707b42
Koray Dundar <koraydundar_40@hotmail.com>
2019-08-29 00:19:45 +0300

    mapped through links in navlink state

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'gatsby';
+
 import './navbar.css'
 
 class NavbarLinks extends Component {
+    state= {
+        links: [
+            {
+                id: 0,
+                path: '/',
+                name: 'home'
+            },
+            {
+                id: 1,
+                path: '/about',
+                name: 'about'
+            },
+            {
+                id: 2,
+                path: '/menu',
+                name: 'menu'
+            },
+            {
+                id: 3,
+                path: '/contact',
+                name: 'contact'
+            }
+        ]
+    }
     render() {
         return (
-            <div>
-                content from navbar links
-            </div>
-        );
+            <ul className="link-wrapper" open={this.props.navbarOpen}>
+                {this.state.links.map(link => {
+                    return (
+                        <li key={link.id}>
+                            <Link to={link.path} className="nav-link">{link.name}</Link>
+                        </li>
+                    )     
+                })}
+            </ul>
+        )
     }
 }
 
