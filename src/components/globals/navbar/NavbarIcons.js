80bc096f030f5e316bb32dd1748fce5a8ff3b5da
Koray Dundar <koraydundar_40@hotmail.com>
2019-09-15 14:54:44 +0300

    completed navbar icons component

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import {FaInstagram, FaTwitter, FaFacebook} from 'react-icons/fa'
+
 import './navbar.css'
 
+
 class NavbarIcons extends Component {
+    state={
+        icons: [
+            {
+                id: 1, 
+                icon: <FaFacebook className="icon facebook-icon" />, 
+                path: 'https://www.facebook.com',
+            },
+            {
+                id: 2,
+                icon: <FaTwitter className="icon twitter-icon" />,
+                path: 'https://www.twitter.com',
+            },
+            {
+                id: 3,
+                icon: <FaInstagram className="icon instagram-icon" />,
+                path: 'https://www.instagram.com',
+            }
+        ]
+    }
     render() {
         return (
-            <div>
-                content from navbar icons
+            <div className="icon-wrapper">
+                {this.state.icons.map(item =>{
+                    return ( 
+                            <a 
+                                href={item.path} 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                            > 
+                                {item.icon}
+                            </a>
+                    )
+                })}
             </div>
         );
     }
