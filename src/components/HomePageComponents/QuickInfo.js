7b848c21d583ea76ab0a69466069d18a74f88e9d
Koray Dundar <koraydundar_40@hotmail.com>
2019-10-07 10:27:14 +0300

    completed quick info section

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import Section from '../globals/section/section'
 import Title from '../globals/title/title'
 import { SectionButton } from '../globals/button/Button';
+import { Link } from 'gatsby'
+import './quickInfo.css'
 
 class QuickInfo extends Component {
     render() {
         return (
             <Section>
                 <Title message="I am a message" title="I am a title" />
-                <SectionButton>about</SectionButton>
+                <div className="quick-info-wrapper">
+                    <p className="text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <Link to="/about/" style={{ textDecoration: 'none' }}>
+                        <SectionButton style={{ margin: '2rem auto' }}>about</SectionButton>
+                    </Link>
+                </div>
             </Section>
         );
     }
